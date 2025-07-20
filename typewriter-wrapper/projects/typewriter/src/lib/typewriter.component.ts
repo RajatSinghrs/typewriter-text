4def8f9803c00c8d1b916b715a221e71babc4b29
RajatSinghrs <rajatsingh265@gmail.com>
2025-07-20 22:28:25 +0530

    setup for angular package

---
+++ typewriter-wrapper/projects/typewriter/src/lib/typewriter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-typewriter',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      typewriter works!
+    </p>
+  `,
+  styles: ``
+})
+export class TypewriterComponent {
+
+}
---
+++ typewriter-wrapper/projects/typewriter/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of typewriter
+ */
+
+export * from './lib/typewriter.service';
+export * from './lib/typewriter.component';
